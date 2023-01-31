43af08b5e42cf7d6b5e7ddd47cd5e575f16802e0
pabloalmansa <pabloalmansa.t@gmail.com>
2023-01-31 12:18:25 +0100

    VQ cleaned, has a type error

--- src/VQ.py
+++ src/VQ.py
@@ -9,6 +9,7 @@ import numpy as np
 import gzip
 import logging
 import main
+import sklearn as cluster
 
 # pip install "scalar_quantization @ git+https://github.com/vicente-gonzalez-ruiz/scalar_quantization"
 from scalar_quantization.LloydMax_quantization import LloydMax_Quantizer as Quantizer
@@ -16,7 +17,13 @@ from scalar_quantization.LloydMax_quantization import name as quantizer_name
 
 import PNG as EC # Entropy Coding
 
+#Default value
+num_clusters = 6
+
 EC.parser_encode.add_argument("-q", "--QSS", type=EC.int_or_str, help=f"Quantization step size (default: 32)", default=32)
+EC.parser_encode.add_argument("-b", "--num-bits", type=EC.int_or_str, help=f"number of bits to use on the quantified image", default=num_clusters)
+
+num_clusters = np.power(2, num_clusters)
 
 class CoDec(EC.CoDec):
     
@@ -26,41 +33,38 @@ class CoDec(EC.CoDec):
     def quantize(self, img):
         with open(f"{self.args.output}_QSS.txt", 'w') as f:
             f.write(f"{self.args.QSS}")
-        self.output_bytes = 1 # We suppose that the representation of the QSS requires 1 byte
-        if len(img.shape) < 3:
-            extended_img = np.expand_dims(img, axis=2)
-        else:
-            extended_img = img
-        k = np.empty_like(extended_img)
-        for c in range(extended_img.shape[2]):
-            histogram_img, bin_edges_img = np.histogram(extended_img[..., c], bins=256, range=(0, 256))
-            histogram_img += 1 # Bins cannot be zeroed
-            self.Q = Quantizer(Q_step=self.args.QSS, counts=histogram_img)
-            centroids = self.Q.get_representation_levels()
-            with gzip.GzipFile(f"{self.args.output}_centroids_{c}.gz", "w") as f:
-                np.save(file=f, arr=centroids)
-            len_codebook = os.path.getsize(f"{self.args.output}_centroids_{c}.gz")
-            self.output_bytes += len_codebook
-            k[..., c] = self.Q.encode(extended_img[..., c])
-        return k
+
+        x = img.reshape(img)
+        kmeans = cluster.KNmeans(n_clusters = num_clusters, n_init=4, random_state=5)
+        kmeans.fit(x)
+        centroids = kmeans.cluster_centers.squeeze()
+        labels = kmeans.labels_
+        image_compressed = np.choose(labels, centroids).reshape(img.shape)
+
+        return image_compressed
 
     def dequantize(self, k):
 
-        # We ahve to undo the operatino
-        with open(f"{self.args.input}_QSS.txt", 'r') as f:
-            QSS = int(f.read())
-        if len(k.shape) < 3:
-            extended_k = np.expand_dims(k, axis=2)
-        else:
-            extended_k = k
-        y = np.empty_like(extended_k)
-        for c in range(y.shape[2]):
-            with gzip.GzipFile(f"{self.args.input}_centroids_{c}.gz", "r") as f:
-                centroids = np.load(file=f)
-            self.Q = Quantizer(Q_step=QSS, counts=np.ones(shape=256))
-            self.Q.set_representation_levels(centroids)
-            y[..., c] = self.Q.decode(extended_k[..., c])
-        return y
+        # We have to undo the operatino
+
+
+        #but how
+
+        
+    #   with open(f"{self.args.input}_QSS.txt", 'r') as f:
+    #       QSS = int(f.read())
+    #   if len(k.shape) < 3:
+    #       extended_k = np.expand_dims(k, axis=2)
+    #   else:
+    #       extended_k = k
+    #   y = np.empty_like(extended_k)
+    #   for c in range(y.shape[2]):
+    #       with gzip.GzipFile(f"{self.args.input}_centroids_{c}.gz", "r") as f:
+    #           centroids = np.load(file=f)
+    #       self.Q = Quantizer(Q_step=QSS, counts=np.ones(shape=256))
+    #       self.Q.set_representation_levels(centroids)
+    #       y[..., c] = self.Q.decode(extended_k[..., c])
+        return 0
         
     def encode(self):
         '''Read an image, quantize the image, and save it.'''
