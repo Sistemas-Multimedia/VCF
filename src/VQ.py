fcf9a8018ee109e804581c8e8134f0cdb436b018
pabloalmansa <pabloalmansa.t@gmail.com>
2023-02-01 12:43:56 +0100

    VQ.py cleaned

--- src/VQ.py
+++ src/VQ.py
@@ -16,10 +16,6 @@ import PNG as EC # Entropy Coding
 
 #Default value
 num_clusters = 6
-
-EC.parser_encode.add_argument("-q", "--QSS", type=EC.int_or_str, help=f"Quantization step size (default: 32)", default=32)
-EC.parser_encode.add_argument("-b", "--num-bits", type=EC.int_or_str, help=f"number of bits to use on the quantified image", default=num_clusters)
-
 num_clusters = np.power(2, num_clusters)
 
 class CoDec(EC.CoDec):
@@ -28,8 +24,6 @@ class CoDec(EC.CoDec):
         super().__init__(args)
 
     def quantize(self, img):
-        with open(f"{self.args.output}_QSS.txt", 'w') as f:
-            f.write(f"{self.args.QSS}")
         x = img
         x = x.reshape((-1, 1))
         kmeans = cluster.KMeans(n_clusters = num_clusters, n_init=4, random_state=5)
@@ -42,32 +36,11 @@ class CoDec(EC.CoDec):
 
     def dequantize(self, k):
 
-        # We have to undo the operatino
-
-
-        #but how
-
-        
-    #   with open(f"{self.args.input}_QSS.txt", 'r') as f:
-    #       QSS = int(f.read())
-    #   if len(k.shape) < 3:
-    #       extended_k = np.expand_dims(k, axis=2)
-    #   else:
-    #       extended_k = k
-    #   y = np.empty_like(extended_k)
-    #   for c in range(y.shape[2]):
-    #       with gzip.GzipFile(f"{self.args.input}_centroids_{c}.gz", "r") as f:
-    #           centroids = np.load(file=f)
-    #       self.Q = Quantizer(Q_step=QSS, counts=np.ones(shape=256))
-    #       self.Q.set_representation_levels(centroids)
-    #       y[..., c] = self.Q.decode(extended_k[..., c])
-        return 0
+        #Not-Implemented
+        return k
         
     def encode(self):
         '''Read an image, quantize the image, and save it.'''
-        # We have to perform vector (Going to implement learning VQ)
-        #STEP 1. Inicializa los pesos
-        #STEP 2. Sele
 
         img = self.read()
         k = self.quantize(img)
@@ -76,20 +49,11 @@ class CoDec(EC.CoDec):
         return rate
 
     def decode(self):
-        #k = io.imread(self.args.input)
         k = self.read()
         y = self.dequantize(k)
         self.write(y)
         rate = (self.input_bytes*8)/(k.shape[0]*k.shape[1])
         return rate
-
-    def read(self):
-
-        file="/tmp/vq-entry.png"
-        io.imsave(file, self.read_fn(self.args.input))
-        
-        self.input_bytes = os.path.getsize(file)
-        return imageio.imread(file)
 
 if __name__ == "__main__":
     quantizer_name = "Vectorial Q. KN"
