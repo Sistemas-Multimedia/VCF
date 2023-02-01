23bb6b7a87a83e644ffd27f0db61be04b8728172
pabloalmansa <pabloalmansa.t@gmail.com>
2023-02-01 23:21:30 +0100

    Now VQ works with blocks

--- src/VQ.py
+++ src/VQ.py
@@ -13,10 +13,15 @@ from sklearn import cluster
 import imageio.v2 as imageio
 # pip install "scalar_quantization @ git+https://github.com/vicente-gonzalez-ruiz/scalar_quantization"
 import PNG as EC # Entropy Coding
+import cv2 as cv
 
 #Default value
-num_clusters = 6
-num_clusters = np.power(2, num_clusters)
+
+block_side = 2
+block_height = block_side
+block_width = block_side
+n_components = 3 #RGB
+block_length = block_height * block_width * n_components
 
 class CoDec(EC.CoDec):
     
@@ -24,36 +29,44 @@ class CoDec(EC.CoDec):
         super().__init__(args)
 
     def quantize(self, img):
-        x = img
-        x = x.reshape((-1, 1))
-        kmeans = cluster.KMeans(n_clusters = num_clusters, n_init=4, random_state=5)
-        kmeans.fit(x)
-        compressed_image = kmeans.cluster_centers_[kmeans.labels_]
-        compressed_image = np.clip(compressed_image.astype('uint8'), 0, 255)
-        compressed_image = compressed_image.reshape(img.shape)
+        blocks = []
+        #print(np.round(img.shape[0] * img.shape[1] / (block_height * block_length)).astype(np.int16))
+        n_clusters = 256  # Number of bins
+        #kmeans = cluster.KMeans(n_clusters = n_clusters, n_init=4, random_state=5)
+        kmeans = cluster.KMeans(init="k-means++", n_clusters=n_clusters, n_init=1)
+        for i in range(0, img.shape[0], block_width):
+            for j in range(0, img.shape[1], block_height):
+                blocks.append(np.reshape(img[i:i + block_width, j:j + block_height], block_length))
 
-        return compressed_image
+        blocks = np.asarray(blocks).astype(float)
+        kmeans.fit(blocks)
+        centroids = kmeans.cluster_centers_.squeeze().astype(np.uint8)
+            
+        labels = kmeans.labels_
+        labels = labels.reshape(img.shape[0]//block_height, img.shape[1]//block_width)
+        
+        img_dequantized = np.empty_like(img)
+        for i in range(0, img.shape[0], block_width):
+            for j in range(0, img.shape[1], block_height):
+                img_dequantized[i:i + block_width, j:j + block_height] = centroids[labels[i//block_width,j//block_height]].reshape(block_height, block_width, n_components)
+                
+        return img_dequantized        
 
-    def dequantize(self, k):
-
-        #Not-Implemented
-        return k
+    def dequantize(self, img, labels, centroids):
+        #NOT-IMPLEMENTED
+        return 0
         
     def encode(self):
         '''Read an image, quantize the image, and save it.'''
-
         img = self.read()
-        k = self.quantize(img)
+        k=self.quantize(img)
         self.write(k)
         rate = (self.output_bytes*8)/(img.shape[0]*img.shape[1])
         return rate
 
     def decode(self):
-        k = self.read()
-        y = self.dequantize(k)
-        self.write(y)
-        rate = (self.input_bytes*8)/(k.shape[0]*k.shape[1])
-        return rate
+        #NOT-IMPLEMENTED
+        return 0
 
 if __name__ == "__main__":
     quantizer_name = "Vectorial Q. KN"
