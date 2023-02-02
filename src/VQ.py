67f9ff59fcbcc2638c1950e330324748c10c1bef
pabloalmansa <pabloalmansa.t@gmail.com>
2023-02-02 15:08:01 +0100

    Both Labels and Centroids are written as images. config contains valuable information about needed transformations. Depth is 4... should be 3

--- src/VQ.py
+++ src/VQ.py
@@ -14,14 +14,19 @@ import imageio.v2 as imageio
 # pip install "scalar_quantization @ git+https://github.com/vicente-gonzalez-ruiz/scalar_quantization"
 import PNG as EC # Entropy Coding
 import cv2 as cv
-
+from PIL import Image
 #Default value
 
-block_side = 2
+bits_per_block = 1
+block_side = np.power(2, bits_per_block)
 block_height = block_side
 block_width = block_side
 n_components = 3 #RGB
 block_length = block_height * block_width * n_components
+
+dir_label="/tmp/_label.png"
+dir_centroids="/tmp/_centroids.png"
+dir_config="/tmp/_config.txt"
 
 class CoDec(EC.CoDec):
     
@@ -30,43 +35,74 @@ class CoDec(EC.CoDec):
 
     def quantize(self, img):
         blocks = []
-        #print(np.round(img.shape[0] * img.shape[1] / (block_height * block_length)).astype(np.int16))
-        n_clusters = 256  # Number of bins
-        #kmeans = cluster.KMeans(n_clusters = n_clusters, n_init=4, random_state=5)
-        kmeans = cluster.KMeans(init="k-means++", n_clusters=n_clusters, n_init=1)
+
         for i in range(0, img.shape[0], block_width):
             for j in range(0, img.shape[1], block_height):
                 blocks.append(np.reshape(img[i:i + block_width, j:j + block_height], block_length))
 
-        blocks = np.asarray(blocks).astype(float)
+        blocks = np.asarray(blocks).astype(int)
+        n_clusters = 256 # number of centroids
+        if(len(blocks) < n_clusters):
+            print('\033[91m' + "Warning: Must reduce number of clusters. Image not big enough | too much pixel reducction" + '\033[0m')
+            exit(-1)
+
+        kmeans = cluster.KMeans(init="k-means++", n_clusters=n_clusters, n_init=1)
         kmeans.fit(blocks)
         centroids = kmeans.cluster_centers_.squeeze().astype(np.uint8)
-            
-        labels = kmeans.labels_
-        labels = labels.reshape(img.shape[0]//block_height, img.shape[1]//block_width)
+        labels=kmeans.labels_
+        print(img.shape[0])
+        #Save original parameters
+        sizes = [[],[],[]]
+        sizes[0] = [img.shape[0], img.shape[1], img.shape[2]]
+        sizes[1] = [centroids.shape[0],centroids.shape[1], 0]
+        sizes[2] = [labels.shape[0], 0, 0]
         
-        img_dequantized = np.empty_like(img)
-        for i in range(0, img.shape[0], block_width):
-            for j in range(0, img.shape[1], block_height):
+        #Transform to reduce space in disck
+        centroids = centroids.flatten()
+
+        ## Save on disk 
+        
+        #np.savetxt(dir_label, labels.astype(int), fmt='%i', delimiter=" ")
+        im_label = Image.fromarray(labels)
+        im_label.save(dir_label)
+        #np.savetxt(dir_centroids, centroids.astype(int), fmt='%i', delimiter=" ")
+        im_centroids = Image.fromarray(centroids)
+        im_centroids.save(dir_centroids)
+        np.savetxt(dir_config, sizes, fmt='%i', delimiter=" ")
+        
+        return None
+
+    def dequantize(self, sizes, labels, centroids):
+        #Rebuiling data
+        labels = labels.reshape(sizes[0][0]//block_height, sizes[0][1]//block_width)
+        centroids = centroids.reshape(sizes[1][0], sizes[1][1])
+        #Use data
+        img_dequantized = np.empty([sizes[0][0], sizes[0][1], sizes[0][2]], dtype=int)
+        for i in range(0, sizes[0][0], block_width):
+            for j in range(0, sizes[0][1], block_height):
                 img_dequantized[i:i + block_width, j:j + block_height] = centroids[labels[i//block_width,j//block_height]].reshape(block_height, block_width, n_components)
                 
-        return img_dequantized        
-
-    def dequantize(self, img, labels, centroids):
-        #NOT-IMPLEMENTED
-        return 0
+        return img_dequantized 
         
     def encode(self):
         '''Read an image, quantize the image, and save it.'''
         img = self.read()
-        k=self.quantize(img)
-        self.write(k)
-        rate = (self.output_bytes*8)/(img.shape[0]*img.shape[1])
+        self.quantize(img)
+        self.output_bytes = os.stat(dir_centroids).st_size + os.stat(dir_label).st_size + os.stat(dir_config).st_size
+        rate = (self.input_bytes*8)/(img.shape[0]*img.shape[1])
+        print('\033[92m'+"All saved files together weights {}% compared to the original image".format(self.output_bytes/self.input_bytes*100)+'\033[0m')
         return rate
 
     def decode(self):
-        #NOT-IMPLEMENTED
-        return 0
+        img_la = Image.open(dir_label)
+        label = np.asarray(img_la)
+        img_cen = Image.open(dir_centroids)
+        centroids = np.asarray(img_cen)
+        sizes=np.loadtxt(dir_config, dtype=int)
+        k=self.dequantize(sizes=sizes, labels=label, centroids=centroids)
+        self.write(k)
+        rate = (self.output_bytes*8)/(img.shape[0]*img.shape[1])
+        return rate
 
 if __name__ == "__main__":
     quantizer_name = "Vectorial Q. KN"
