b3e28fa77f43e33bd768817eb7311b584666223f
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2025-03-21 10:08:45 +0100

    .

--- src/YCrCb.py
+++ src/YCrCb.py
@@ -41,15 +41,15 @@ class CoDec(Q.CoDec):
         y = y.astype(np.uint8)
         return y
 
-    def encode(self):
+    def encode_fn(self, in_fn, out_fn):
         logging.debug("trace")
-        img = self.encode_read()#.astype(np.int16)
+        img = self.encode_read_fn(in_fn)#.astype(np.int16)
         #print("----------------->", img.dtype)
         #img -= 128
         #img_128 = img.astype(np.int16) - 128
         #img = img.astype(np.uint8)
         YCrCb_img = from_RGB(img)
-        k = self.quantize(YCrCb_img)
+        k = self.quantize_fn(YCrCb_img, out_fn)
         #k += 128
         if np.max(k) > 255:
             logging.warning(f"k[{np.unravel_index(np.argmax(k),k.shape)}]={np.max(k)}")
@@ -58,19 +58,20 @@ class CoDec(Q.CoDec):
         #k = k.astype(np.uint8)
         k = k.astype(np.uint16)
         
-        compressed_k = self.compress(k)
-        self.encode_write(compressed_k)
+        compressed_k = self.compress_fn(k, in_fn)
+        output_size = self.encode_write_fn(compressed_k, out_fn)
         #self.BPP = (self.output_bytes*8)/(img.shape[0]*img.shape[1])
         #logging.info(f"BPP = {BPP}")
+        return output_size
 
-    def decode(self):
+    def decode_fn(self, in_fn, out_fn):
         logging.debug("trace")
-        compressed_k = self.decode_read()
-        k = self.decompress(compressed_k)
+        compressed_k = self.decode_read_fn(in_fn)
+        k = self.decompress_fn(compressed_k, in_fn)
         #k = k.astype(np.int16)
         #k -= 128
         #k = self.read()
-        YCrCb_y = self.dequantize(k)
+        YCrCb_y = self.dequantize_fn(k, in_fn)
         #y_128 = to_RGB(YCoCg_y.astype(np.int16))
         YCrCb_y = YCrCb_y.astype(np.uint8)
         y = to_RGB(YCrCb_y)
@@ -82,10 +83,11 @@ class CoDec(Q.CoDec):
             logging.warning(f"y[{np.unravel_index(np.argmin(y),y.shape)}]={np.min(y)}")
         y = np.clip(y, 0, 255).astype(np.uint8)
         y = Q.denoiser.CoDec.filter(self, y)
-        self.decode_write(y)
+        output_size = self.decode_write_fn(y, out_fn)
         #self.BPP = (self.input_bytes*8)/(k.shape[0]*k.shape[1])
         #RMSE = distortion.RMSE(self.encode_read(), y)
         #logging.info(f"RMSE = {RMSE}")
+        return output_size
 
 if __name__ == "__main__":
     main.main(parser.parser, logging, CoDec)
