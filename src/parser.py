66e12aba5c67c15ebe35a75e0dd41036b6212b1e
eim344 <eim344@inlumine.ual.es>
2026-01-29 20:08:48 +0100

    Deshacer cambio

--- src/parser.py
+++ src/parser.py
@@ -15,8 +15,34 @@ def decode(codec):
     return codec.decode()
 
 class CustomArgumentParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,
+                 prog=None,
+                 usage=None,
+                 description=None,
+                 epilog=None,
+                 parents=[],
+                 formatter_class=argparse.HelpFormatter,
+                 prefix_chars='-',
+                 fromfile_prefix_chars=None,
+                 argument_default=None,
+                 conflict_handler='error',
+                 add_help=True,
+                 allow_abbrev=True,
+                 exit_on_error=True):
+        #print(description)
+        super().__init__(prog,
+                         usage,
+                         description,
+                         epilog,
+                         parents,
+                         formatter_class,
+                         prefix_chars,
+                         fromfile_prefix_chars,
+                         argument_default,
+                         conflict_handler,
+                         add_help,
+                         allow_abbrev,
+                         exit_on_error)
     def exit(self, status=0, message=None):
         if message:
             self._print_message(message, None)
